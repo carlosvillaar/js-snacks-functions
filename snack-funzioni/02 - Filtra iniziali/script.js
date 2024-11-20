15558a248e126b71467b9953513020d20537aa9a
carlosvillaar <carlosvillar004@gmail.com>
2024-11-20 18:21:57 +0100

    feat: add second function

--- snack-funzioni/02 - Filtra iniziali/script.js
+++ snack-funzioni/02 - Filtra iniziali/script.js
@@ -5,9 +5,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function incominciaPer(array, letter){
+  const newArray = [];
+  for (let i = 0; i < array.length; i++){
+    if (letter === array[i].at(0)){
+      newArray.push(array[i])
+    }
+  }
+  return newArray
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(incominciaPer(names, `A`))
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
