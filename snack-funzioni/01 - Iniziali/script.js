51c868bcc6af186722f4a4deefb94171be79689f
carlosvillaar <carlosvillar004@gmail.com>
2024-11-20 18:14:03 +0100

    feat: add first function

--- snack-funzioni/01 - Iniziali/script.js
+++ snack-funzioni/01 - Iniziali/script.js
@@ -4,9 +4,24 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function initialWords(mioArray) {
+
+  const newArray = [];
+
+  let iniziali;
+  
+  for (let i = 0; i < mioArray.length; i++){
+    iniziali = mioArray[i].at(0);
+    newArray.push(iniziali);
+  }
+
+  return newArray;
+
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(initialWords(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
